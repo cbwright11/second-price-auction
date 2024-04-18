58caedacb2a986a4607484b2b95cb1eb9867a2ab
Cameron Wright <cameronwright@Camerons-MacBook-Air.local>
2024-04-18 12:34:27 -0700

    add files initially

---
+++ auction_wright.py
@@ -0,0 +1,210 @@
+# from bidder_wright import Bidder
+import numpy as np
+# import matplotlib.pyplot as plt
+
+class User():
+    """This class creates users that are to be shown ads"""
+    
+    def __init__(self):
+        self.__probability = np.random.uniform(0, 1)
+    
+    def show_ad(self):
+        """Returns a boolean: True if the user clicks on the add, False otherwise. Remember that the user's 
+        likelihood of clicking is not influenced at all by who the specific bidder is that shows the ad. All that
+        matters is the user's secret probability of clicking"""
+        
+        ad_id = np.random.uniform(0, 1)
+        if ad_id <= self.__probability:
+            return True
+        else:
+            return False
+    
+    # this method is just for development purposes, I might remove
+    def get_prob(self):
+        return round(self.__probability, 2)
+
+
+class Auction():
+    """This class creates Auction objects. So a given auction object will have info on the number of bidders
+    and the number of users in the game. You can call the .execute_rounds() method as many times as you want rounds  
+    in your auction. 
+    Attributes are:
+    bidders
+    users
+    balances
+    winning_prices
+    wins_by_bidder
+    bids_by_user
+    winning_price_by_user
+    num_rounds
+    balances_by_round"""
+    
+    def __init__(self, users, bidders):
+        self.bidders = bidders
+        self.users = users
+        self.balances = {bidder:0 for bidder in self.bidders} # finances will be completely handled in Auction class
+        # self.winning_prices = []
+        # self.wins_successes_by_bidder = {bidder:[0,0] for bidder in self.bidders} # keep track of how many wins each bidder has
+        # self.bids_by_user = {i:[0,0] for i in range(len(users))} # keep track of the average of EVERY bid by user
+        # self.winning_price_by_user = {i:[0,0] for i in range(len(users))} # keep track of the average of WINNING PRICE by user
+        # self.num_rounds = 0
+        # self.balances_by_round = {bidder:[0] for bidder in self.bidders}
+        # self.secret_probs = [user.get_prob() for user in users]
+        # self.rounds = {}
+    
+    def execute_round(self):
+        """This should execute all the steps within a single round of the game"""
+        
+        # choose a user at random
+        user = np.random.randint(0, len(self.users)) 
+        
+        bids = {} # store the bids in a dictionary --> bidder is the key, their bid is the value
+        # call bid for each bidder using the id of the chosen user
+        for bidder in self.bidders:
+            bid = bidder.bid(user)
+            bids[bidder] = bid
+        
+        # loop through the bids dictionary to figure out the winning_bidder and the second_highest_bid
+        max_bid = 0            # only for the purpose of determining winning_bidder
+        second_highest_bid = 0 # starts at zero
+        for bidder,bid in bids.items():
+            if bid > max_bid:
+                max_bid = bid
+                winning_bidder = bidder     # set/update winning bidder
+            elif bid > second_highest_bid:
+                second_highest_bid = bid    # update second_highest price
+            
+        # show the ad to the user
+        ad_outcome = self.users[user].show_ad()
+        
+        # loop through the bidders to notify each of them of the outcome of the auction round
+        # also adjust their balances accordingly
+        for bidder in self.bidders:
+            # the winner also gets to find out the result of showing the add to the user
+            if bidder == winning_bidder:
+                bidder.notify(True, second_highest_bid, ad_outcome) 
+                self.balances[bidder] -= second_highest_bid
+#                 bidder.balances -= second_highest_bid
+                if ad_outcome:
+                    self.balances[bidder] += 1
+#                     bidder.balances += 1
+                #     self.wins_successes_by_bidder[bidder][1] += 1 # just for diagnostics, not crucial
+                # self.wins_successes_by_bidder[bidder][0] += 1 # just for diagnostics, not crucial
+            else:
+                bidder.notify(False, second_highest_bid, None)
+        
+        # # following four blocks are just for diagnostics, not crucial
+        # bids_mean = np.mean([bid for bid in bids.values()]) # take the average of the bids for this round
+        # # and update the instance attribute bids_by_user to update the average bid for them
+        # self.bids_by_user[user][0] = (self.bids_by_user[user][0] + bids_mean)/(self.bids_by_user[user][1] + 1)
+        # self.bids_by_user[user][1] += 1 # increment the number of rounds that they've been selected by 1
+        
+        # # update the average of the winning prices for each user
+        # self.winning_price_by_user[user][0] = (self.winning_price_by_user[user][0] + second_highest_bid)/(self.winning_price_by_user[user][1] + 1)
+        # self.winning_price_by_user[user][1] += 1 # increment the number of rounds they've been selected by 1
+        
+        # # update balances by round to see the history of the bidders balances over the course of the game
+        # for bidder in self.bidders: 
+        #     self.balances_by_round[bidder].append(self.balances[bidder])
+        
+        # # update num_rounds
+        # self.num_rounds += 1
+        
+        # self.rounds[self.num_rounds] = [user, self.secret_probs[user], ad_outcome]
+        
+        # self.winning_prices.append(second_highest_bid)
+                
+                
+    # # optional
+    # def plot_history(self, info_type="balance_history"):
+    #     """Create a visual representation of how the auction has proceeded. 
+    #     info_type parameter determines what type of graph you are shown. Options are:
+    #     balance_history
+    #     wins_by_bidder
+    #     winning_prices
+    #     bids_by_user
+    #     winning_price_by_user
+    #     summary_by_round
+    #     """
+        
+    #     if info_type == "balance_history":
+    #         x = np.arange(self.num_rounds + 1)
+    #         # create several lists, each representing the history of each bidder's balance over the course of the game
+    #         y = [[balance for balance in self.balances_by_round[bidder]] for bidder in self.bidders]
+    #         for bidder in y:
+    #             plt.plot(x,bidder)
+    #         plt.legend(["bidder" + str(i) for i in range(len(y))])
+    #         plt.xlabel("round")
+    #         plt.ylabel("balance")
+    #         plt.title("history of balances by user over the rounds")
+    #         plt.show()
+        
+    #     elif info_type == "wins_by_bidder":
+    #         x = ["bidder" + str(i) for i in range(len(self.bidders))]
+    #         num_wins = [self.wins_successes_by_bidder[bidder][0] for bidder in self.bidders]
+    #         num_successes = [self.wins_successes_by_bidder[bidder][1] for bidder in self.bidders]
+            
+    #         w = 0.4
+    #         bar1 = np.arange(len(x))
+    #         bar2 = [i+w for i in bar1]
+            
+    #         plt.bar(bar1, num_wins, w, label="number of wins")
+    #         plt.bar(bar2, num_successes, w, label="number of successes")
+            
+    #         plt.xticks(bar1+w/2, x)
+    #         plt.xlabel("bidder")
+    #         plt.ylabel("wins and successes")
+    #         plt.legend()
+    #         plt.title("Number of wins and successes by bidder")
+    #         plt.show()
+            
+    #     elif info_type == "winning_prices":
+    #         x = np.arange(1, self.num_rounds+1)
+    #         y = [winning_price for winning_price in self.winning_prices]
+    #         plt.scatter(x,y)
+    #         plt.xlabel("round number")
+    #         plt.ylabel("price")
+    #         plt.title("winning prices by rounds")
+    #         plt.show()
+            
+    #     elif info_type == "bids_by_user":
+    #         x = np.arange(len(self.users))
+    #         y = [pair[0] for pair in self.bids_by_user.values()]
+    #         z = [prob for prob in self.secret_probs]
+            
+    #         w = 0.4
+    #         bar1 = np.arange(len(x))
+    #         bar2 = [i+w for i in bar1]
+            
+    #         plt.bar(bar1, y, w, label="average of bids")
+    #         plt.bar(bar2, z, w, label="secret probabilities")
+            
+    #         plt.xticks(bar1+w/2, x)
+            
+    #         plt.xlabel("user_id")
+    #         plt.ylabel("bid amount and secret probability")
+    #         plt.title("comparison of bidders attempts per user vs users' actual clicking likelihoods")
+    #         plt.legend()
+    #         plt.show()
+            
+    #     elif info_type == "winning_price_by_user":
+    #         x = np.arange(len(self.users))
+    #         y = [pair[0] for pair in self.winning_price_by_user.values()]
+    #         plt.bar(x,y)
+    #         plt.xlabel("user_id")
+    #         plt.ylabel("bid amount")
+    #         plt.title("average of the winning bids in each round by user")
+    #         plt.show()
+            
+    #     elif info_type == "summary_by_round":
+    #         # I want to know which user was selected
+    #         # I want to know their clicking likelihood
+    #         # I want to know whether they clicked
+    #         # I want to know which bidder won **
+    #         # I want to know the winning price
+    #         return self.rounds
+            
+            
+            
+            
+
---
+++ bidder_wright.py
@@ -0,0 +1,50 @@
+
+class Bidder():
+    """This class creates bidders equipped with knowledge of the number of users in the auction and the number 
+    of auction rounds to be played"""
+    
+    def __init__(self, num_users, num_rounds, alpha = 0.1, aggressiveness = 0.85, under_cut = 0):
+        # third entry is the estimated clicking likelihood of the user
+        self.prob_estimates = {i:[0,0,0] for i in range(num_users)}
+        self.current_user = 0 
+        self.alpha = alpha
+        self.num_users = num_users
+        self.num_rounds = num_rounds
+        self.under_cut = under_cut
+        self.current_round = 0
+        self.aggressiveness = aggressiveness
+    
+    def bid(self, user_id):
+        """Returns a non-negative amount of money. If you don't wish to bid anything on a given user, this should 
+        return 0.
+        My strategy is to explore for the first `alpha` percent of the game with a bid of `aggressiveness` dollars.
+        Then I enter the exploit phase and bid `under_cut` dollars below my estimate of the user's likelihood of 
+        clicking."""
+        self.current_user = user_id
+
+        if self.current_round / self.num_rounds < self.alpha: # for the first `alpha` percent of the game
+            self.current_round += 1
+            return self.aggressiveness # simply bid `aggressiveness` = 0.6
+        else: # for the rest of the game
+            self.current_round += 1
+            return self.prob_estimates[user_id][2] - self.under_cut # base the bid on the estimated clicking likelihood of the user
+        
+    
+    def notify(self, auction_winner, price, clicked=None):
+        """Used to send info about what happened in a round back to the bidder. auction_winner will be a boolean:
+        True if the bidder won a given round, False otherwise. price will be the amount of the second bid, only the
+        winner pays this. clicked will only take on a value if auction_winner is True (if the bidder won that given
+        round) in which case it will take on a boolean value: True if the user clicked on the ad and False 
+        otherwise"""
+        if auction_winner:        # If the bidder won the auction
+            if clicked:           # Additionally, if the user clicked
+                # update the prob_estimates dictionary for the current user
+                self.prob_estimates[self.current_user][0] += 1
+                self.prob_estimates[self.current_user][1] += 1
+                self.prob_estimates[self.current_user][2] = self.prob_estimates[self.current_user][0] / self.prob_estimates[self.current_user][1]
+            else:  # if the user didn't click
+                # update the prob_estimates dictionary for the current user
+                self.prob_estimates[self.current_user][1] += 1
+                self.prob_estimates[self.current_user][2] = self.prob_estimates[self.current_user][0] / self.prob_estimates[self.current_user][1]
+
+
